0924279a0bbc9ada6a9b3b5b73edfb7b5cee4c16
Haochen Feng <111423875+HaochenFeng0101@users.noreply.github.com>
2025-05-29 13:59:30 +0800

    Delete localSlam/build directory

--- localSlam/build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/haochen/dconstruct/kiss_icp/localSlam/test
-# Build directory: /home/haochen/dconstruct/kiss_icp/localSlam/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../gtest")
-subdirs("localSlam")
